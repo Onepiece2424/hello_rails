727a5a49599ebfa3d30a740b37f77f341980b006
rioto.nagano <kingdom950927@gmail.com>
2022-10-09 05:38:37 +0900

    コールバックする条件の設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -16,10 +16,15 @@ class Book < ApplicationRecord
     end
   end
 
-  after_destroy do
-    Rails.logger.info "Book is deleted: #{self.attributes} "
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes} "
+    Rails.logger.warn "Please check!!"
   end
 
+  def high_price?
+    price >= 5000
+  end
+  
   validate do |book|
     if book.name.include?("exercise")
       book.errors[:name] << "I don't like exercise"

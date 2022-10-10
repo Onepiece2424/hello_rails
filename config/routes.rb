0bfb4ea404cf6ff184a20c1f857b23755045e9da
rioto.nagano <kingdom950927@gmail.com>
2022-10-10 21:37:16 +0900

    profile コントローラとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   root to: "users#index"
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   resources :tasks, :cards, :users, :books, :publishers
+  resource :profile, only: %i{show edit update}
 end

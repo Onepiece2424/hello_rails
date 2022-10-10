0f0544b7f9b23adc0f14da7b3629743597e10e24
rioto.nagano <kingdom950927@gmail.com>
2022-10-10 12:48:21 +0900

    カードの一覧、登録、詳細、編集、削除機能の作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,46 @@
 class CardsController < ApplicationController
   def index
+    @cards = Card.all
   end
 
   def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to cards_path, notice: 'Card data is create!!!'
+    else
+      render :new
+    end
   end
 
   def show
+    @card = Card.find(params[:id])
   end
 
   def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      redirect_to cards_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:name, :last4, :exp_year, :exp_month)
   end
 end

d028f0627c163b9131599189667f31a3acc21721
rioto.nagano <kingdom950927@gmail.com>
2022-10-10 21:03:45 +0900

    publishersのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/new'
-  get 'books/show'
-  get 'books/edit'
   root to: "users#index"
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  resources :tasks, :cards, :users, :books
+  resources :tasks, :cards, :users, :books, :publishers
 end

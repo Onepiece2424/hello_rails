c2d5b0934168fd654ea99747d8a3cbf24a27f162
rioto.nagano <kingdom950927@gmail.com>
2022-10-15 21:06:09 +0900

    reduxの導入と書くファイル群の作成

---
+++ frontend/src/actions/AppActions.jsx
@@ -0,0 +1,29 @@
+// ./actions/AppActions.jsx
+const Actions = {
+  addTodo(value) {
+    return {
+      type: 'ADDTASK',
+      value,
+    }
+  },
+  fixTodo(value) {
+    return {
+      type: 'FIXTASK',
+      value,
+    }
+  },
+  updTodo(value) {
+    return {
+      type: 'UPDTASK',
+      value,
+    }
+  },
+  delTodo(value) {
+    return {
+      type: 'DELTASK',
+      value,
+    }
+  },
+}
+
+export default Actions
---
+++ frontend/src/containers/App.jsx
@@ -0,0 +1,12 @@
+// ./containers/App.jsx
+import {
+  connect,
+} from 'react-redux'
+
+import App from './../components/App'
+
+const mapStateToProps = (state) => {
+  return state
+}
+
+export default connect(mapStateToProps)(App)

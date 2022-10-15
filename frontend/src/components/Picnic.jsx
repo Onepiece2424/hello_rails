0d9a927b3f9222dc084af2eaab946f88c8a4266f
rioto.nagano <kingdom950927@gmail.com>
2022-10-15 10:15:11 +0900

    オブジェクトの省略記法より、設定値名の省略

--- frontend/src/components/Picnic.jsx
+++ frontend/src/components/Picnic.jsx
@@ -3,8 +3,11 @@ import React from "react"
 
 const Picnic = (props) => {
 
+  const {color} = props    // propsを分割代入（props.~をせずにprops.colorを表現できる。）
+
+  // App.jsx（親）のpropsのcolorをPicnic.jsx（子）へ渡す。また、「オブジェクトの省略記法」により、オブジェクトのプロパティ（今回でいうと、contentStyleのcolor）と設定値名（color:color,の後ろのcolor）が同一なので、後ろのcolorを省略することもできる。
   const contentStyle = {
-    color: props.color,     // App.jsx（親）のpropsのcolorをPicnic.jsx（子）へ渡す
+    color,
     fontSize: "40px",
   }
 

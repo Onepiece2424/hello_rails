b73b597a66e753a091053d9bca19340103602d19
rioto.nagano <kingdom950927@gmail.com>
2022-10-19 06:25:55 +0900

    不要な部分の削除

--- frontend/src/reducks/users/actions.js
+++ frontend/src/reducks/users/actions.js
@@ -10,15 +10,3 @@ export const signInAction = (userState) => {
         }
     }
 };
-
-export const SIGN_OUT = "SIGN_OUT";
-export const signOutAction = () => {
-    return {
-        type: "SIGN_OUT",
-        payload: {
-            isSignedIn: false,
-            uid: "",
-            username: ""
-        }
-    }
-};

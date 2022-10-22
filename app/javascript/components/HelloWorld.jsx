d2499ab644354d5fc3a694bca908489521ecf7ed
rioto.nagano <kingdom950927@gmail.com>
2022-10-23 08:59:54 +0900

    map関数を用いて、ユーザーごとのコンテンツ表示

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,29 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
-// class HelloWorld extends React.Component {
-//   render () {
-//     return (
-//       <React.Fragment>
-//         Greeting: {this.props.greeting}
-//       </React.Fragment>
-//     );
-//   }
-// }
 
 const HelloWorld = (props) => {
   const greeting = props.greeting
-  const users = props.users[0]
+  const users = props.users
   return (
     <>
       <h2>Greeting: {greeting}</h2>
-      <h2>ユーザー名:{users.username}</h2>
+      <h2>
+      {users.map((user)=> {
+        return <div>{user.id}:{user.username}</div>
+      })}
+      </h2>
     </>
   )
 }
-
-HelloWorld.propTypes = {
-  greeting: PropTypes.string,
-  user: PropTypes.string
-};
 
 export default HelloWorld

d67cfd05dce46200faacf9b96062d71da11b6932
rioto.nagano <kingdom950927@gmail.com>
2022-10-22 10:15:56 +0900

    コメントアウトの解除

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,16 +1,14 @@
  import React from "react";
- import {Route, Routes, Link} from "react-router-dom";
+ import {Route, Routes} from "react-router-dom";
  import {Login, Home} from "./templates";
 
  const Router = () => {
 
   return (
     <>
-    <Link to="/login">Login</Link>
-    <Link to="/">Home</Link>
     <Routes>
-      <Route exact path='/login' component={<Login />} />
-      <Route exact path='(/)?' component={<Home />} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='(/)?' component={Home} />
     </Routes>
     </>
   )

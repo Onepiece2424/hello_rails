e9f2361430533b755ae11af576503d696c6be29c
rioto.nagano <kingdom950927@gmail.com>
2022-10-22 17:22:20 +0900

    BrowserRouterの削除

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,13 @@ import App from './App';
 import createStore from './reducks/store/store'
 import { Provider } from 'react-redux'
 import * as History from 'history'
-import {BrowserRouter as Router} from 'react-router-dom';
 
 const history = History.createBrowserHistory();
 export const store = createStore(history);   // storeが作られる。
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
       <App />
-    </Router>
   </Provider>,
   document.getElementById('root'),
 )
